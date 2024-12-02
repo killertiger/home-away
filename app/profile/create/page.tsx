facda84eb02368019348ee3120eac7f9d83b00b0
Marcus <moliveira@pinterest.com>
2024-12-02 14:44:25 -0800

    feat: :sparkles: FormInput

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,5 +1,4 @@
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
+
 import { Button } from '@/components/ui/button';
 
 const createProfileAction = async (formData: FormData) => {
@@ -15,10 +14,6 @@ export default function CreateProfilePage() {
             <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
             <div className='border p-8 rounded-md max-w-lg'>
                 <form action={createProfileAction}>
-                    <div className='mb-2'>
-                        <Label htmlFor='firstName'>First Name</Label>
-                        <Input id='firstName' name='firstName' type='text' />
-                    </div>
                     <Button type='submit' size='lg'>Create Profile</Button>
                 </form>
             </div>
---
+++ components/form/FormInput.tsx
@@ -0,0 +1,27 @@
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+
+type FormInputProps = {
+    name: string;
+    type: string;
+    label?: string;
+    defaultValue?: string;
+    placeholder?: string;
+}
+
+export default function FormInput(props: FormInputProps) {
+    const { label, name, type, defaultValue, placeholder } = props;
+
+    return (
+        <div className='mb-2'>
+            <Label htmlFor={name} className='capitalize'>{label || name}</Label>
+            <Input
+                id={name}
+                name={name}
+                type='text'
+                defaultValue={defaultValue}
+                placeholder={placeholder}
+                required />
+        </div>
+    )
+}

2474b42202aa77fc53cfa0bbea593f10f76b6f57
Marcus <moliveira@pinterest.com>
2024-12-02 14:50:30 -0800

    feat: :sparkles: Submit button

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,6 +1,4 @@
 
-import { Button } from '@/components/ui/button';
-
 const createProfileAction = async (formData: FormData) => {
     'use server';
 
@@ -14,7 +12,6 @@ export default function CreateProfilePage() {
             <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
             <div className='border p-8 rounded-md max-w-lg'>
                 <form action={createProfileAction}>
-                    <Button type='submit' size='lg'>Create Profile</Button>
                 </form>
             </div>
         </section>

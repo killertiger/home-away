fa595f028724a587a3442a5e58f4635f367897d8
Marcus <moliveira@pinterest.com>
2024-12-03 12:38:57 -0800

    feat: :sparkles: AmentitiesInput

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -1,3 +1,4 @@
+import AmenitiesInput from '@/components/form/AmenitiesInput'
 import { SubmitButton } from '@/components/form/Buttons'
 import CategoriesInput from '@/components/form/CategoriesInput'
 import CounterInput from '@/components/form/CounterInput'
@@ -48,6 +49,8 @@ export default function CreatePropertyPage() {
                     <CounterInput detail='bedrooms'/>
                     <CounterInput detail='beds'/>
                     <CounterInput detail='baths'/>
+                    <h3 className='text-lg mt-10 mb-6 font-medium'>Amenities</h3>
+                    <AmenitiesInput />
 
                     <SubmitButton text='create rental' className='mt-12' />
                 </FormContainer>

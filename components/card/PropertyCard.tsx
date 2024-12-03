07f1aff7c29737dd03d6e6ebf7e2af756a1d5857
Marcus <moliveira@pinterest.com>
2024-12-03 14:08:21 -0800

    feat: :sparkles: Improve PropertyCard and PropertyList

--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -1,7 +1,43 @@
+import { PropertyCardProps } from '@/utils/types'
+import Link from 'next/link';
 import React from 'react'
+import Image from 'next/image';
+import { formatCurrency } from '@/utils/format';
 
-export default function PropertyCard() {
+export default function PropertyCard({ property }: { property: PropertyCardProps }) {
+  const { name, image, price } = property;
+  const { country, id: propertyId, tagline } = property;
   return (
-    <div>PropertyCard</div>
+    <article className='group relative'>
+      <Link href={`/properties/${propertyId}`}>
+        <div className='relative h-[300px] mb-2 overflow-hidden rounded-md'>
+          <Image
+            src={image}
+            fill
+            sizes='(max-width: 768px) 100vw, 50vw'
+            alt={name}
+            className='rounded-md object-cover transform group-hover:scale-110 transition-transform duration-500'
+          />
+        </div>
+        <div className='flex justify-between items-center'>
+          <h3 className='text-sm font-semibold mt-1'>
+            {name.substring(0, 30)}
+          </h3>
+          {/* property rating */}
+        </div>
+        <p className='text-sm mt-1 text-muted-foreground'>
+          {tagline.substring(0, 40)}
+        </p>
+        <div className='flex justify-between items-center mt-1'>
+          <p className='text-sm mt-1'>
+            <span className='font-semibold'>{formatCurrency(price)} </span>
+          </p>
+          {/* country and flag */}
+        </div>
+      </Link>
+      <div className='absolute top-5 right-5 z-5'>
+        {/* favorite toggle button */}
+      </div>
+    </article>
   )
 }

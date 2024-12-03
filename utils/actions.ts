9a437d4b673af67cf3f21c8469e62b914e277c29
Marcus <moliveira@pinterest.com>
2024-12-03 08:55:02 -0800

    feat:

--- utils/actions.ts
+++ utils/actions.ts
@@ -5,6 +5,19 @@ import { profileSchema } from "./schemas";
 import db from "./db";
 import { redirect } from "next/navigation";
 
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("Please login to create a profile");
+  }
+
+  if (!user.privateMetadata.hasProfile) {
+    redirect("/profile/create");
+  }
+
+  return user;
+};
+
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
@@ -59,4 +72,26 @@ export const fetchProfileImage = async () => {
   });
 
   return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+
+  if (!profile) {
+    redirect("/profile/create");
+  }
+
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: "update profile action" };
 };

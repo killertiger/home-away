d39ed2e356880ea01e49322981453d5caa33f52a
Marcus <moliveira@pinterest.com>
2024-12-03 10:48:02 -0800

    feat: :sparkles: Upload Profile image to supabase

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'img.clerk.com',
-            }
-        ]
-    }
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "srlomnpqdoxptzsmcngi.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;

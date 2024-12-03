3a2a5b5b26e3424b02ac26f2ec9383eb1c12660f
Marcus <moliveira@pinterest.com>
2024-12-03 04:32:26 -0800

    feat: :sparkles: Display user profile image

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import {LuUser2} from 'react-icons/lu';
+import { LuUser2 } from 'react-icons/lu';
+import { fetchProfileImage } from '@/utils/actions';
 
-export default function UserIcon() {
+export default async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+  if (profileImage) {
+    return <img src={profileImage} className='w-6 h-6 rounded-full object-cover' />;
+  }
+
   return <LuUser2 className='w-6 h-6 bg-primary rounded-full text-white' />;
 }

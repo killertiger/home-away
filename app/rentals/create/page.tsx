dacba53f21fb2de2b74dde148bb6e7d68ead3a13
Marcus <moliveira@pinterest.com>
2024-12-03 12:14:20 -0800

    feat: :sparkles: ConterInput

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -1,5 +1,6 @@
 import { SubmitButton } from '@/components/form/Buttons'
 import CategoriesInput from '@/components/form/CategoriesInput'
+import CounterInput from '@/components/form/CounterInput'
 import CountriesInput from '@/components/form/CountriesInput'
 import FormContainer from '@/components/form/FormContainer'
 import FormInput from '@/components/form/FormInput'
@@ -40,6 +41,14 @@ export default function CreatePropertyPage() {
                         <CountriesInput />
                         <ImageInput />
                     </div>
+                    <h3 className='text-lg mt-8 mb-4 font-medium'>
+                        Accommodation Details
+                    </h3>
+                    <CounterInput detail='guests'/>
+                    <CounterInput detail='bedrooms'/>
+                    <CounterInput detail='beds'/>
+                    <CounterInput detail='baths'/>
+
                     <SubmitButton text='create rental' className='mt-12' />
                 </FormContainer>
             </div>

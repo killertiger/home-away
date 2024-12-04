bea55e5eddd34745d29892a02f2f5d4d585d3a7a
Marcus <moliveira@pinterest.com>
2024-12-03 17:06:27 -0800

    feat: :sparkles: Ask for user to signin when click on favorite button

--- components/card/FavoriteToggleButton.tsx
+++ components/card/FavoriteToggleButton.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { FaHeart } from 'react-icons/fa';
 import { Button } from '../ui/button';
+import { auth } from '@clerk/nextjs/server';
+import { CardSignInButton } from '../form/Buttons';
 
-export default function FavoriteToggleButton({propertyId}: {propertyId: string}) {
+export default function FavoriteToggleButton({ propertyId }: { propertyId: string }) {
+  const { userId } = auth();
+  if (!userId) {
+    return <CardSignInButton />;
+  }
+
   return <Button size='icon' variant='outline' className='p-2 cursor-pointer'><FaHeart /></Button>
 }
--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -3,6 +3,8 @@
 import { useFormStatus } from "react-dom";
 import { Button } from "../ui/button";
 import { ReloadIcon } from "@radix-ui/react-icons";
+import { SignInButton } from "@clerk/nextjs";
+import { FaRegHeart } from "react-icons/fa";
 
 type btnSize = 'default' | 'lg' | 'sm';
 
@@ -25,4 +27,18 @@ export function SubmitButton({ className = '', text = 'submit', size = 'lg' }: S
             <ReloadIcon className="mr-2 h-4 w-4 animate-spin" /> Please wait ...
         </> : text}
     </Button>;
+}
+
+export const CardSignInButton = () => {
+    return <SignInButton mode='modal'>
+        <Button
+            type='button'
+            size='icon'
+            variant='outline'
+            className="p-2 cursor-pointer"
+            asChild
+        >
+            <FaRegHeart />
+        </Button>
+    </SignInButton>
 }

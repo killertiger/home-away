82019d1e87677660ad07562a981aa61fd1b1730a
Marcus <moliveira@pinterest.com>
2024-12-05 02:54:01 -0800

    feat: :sparkles: Avoid booking blocked dates

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,23 +1,40 @@
 'use client';
 
-import { defaultSelected, generateBlockedPeriods } from "@/utils/calendar";
+import { defaultSelected, generateBlockedPeriods, generateDateRange, generateDisabledDates } from "@/utils/calendar";
 import { useEffect, useState } from "react";
 import { DateRange } from "react-day-picker";
 import { Calendar } from "../ui/calendar";
 import { useProperty } from "@/utils/store";
+import { useToast } from "@/hooks/use-toast";
 
 export default function BookingCalendar() {
   const currentDate = new Date();
   const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
   const bookings = useProperty((state) => state.bookings);
+  const { toast } = useToast();
 
   const blockedPeriods = generateBlockedPeriods({
     bookings,
     today: currentDate,
-  })
+  });
+
+  const unavailableDates = generateDisabledDates(blockedPeriods);
 
   useEffect(() => {
+    const selectedRange = generateDateRange(range);
+    const isDisabledDateIncluded = selectedRange.some((date) => {
+      if (unavailableDates[date]) {
+        setRange(defaultSelected);
+        toast({
+          description: 'Some dates are booked. Please select again',
+        });
+        return true;
+      }
+      return false;
+    });
+
     useProperty.setState({ range });
+
   }, [range]);
 
   return (

f6f9c8dc3d80f484eccd1ef8addd49a138c9e54a
Marcus <moliveira@pinterest.com>
2024-12-05 02:30:07 -0800

    feat: :sparkles: Create booking

--- components/booking/ConfirmBooking.tsx
+++ components/booking/ConfirmBooking.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+'use client';
+
+import { useProperty } from "@/utils/store";
+import { SignInButton, useAuth } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+import FormContainer from "../form/FormContainer";
+import { SubmitButton } from "../form/Buttons";
+import { createBookingAction } from "@/utils/actions";
 
 export default function ConfirmBooking() {
+  const { userId } = useAuth();
+  const { propertyId, range } = useProperty((state) => state);
+  const checkIn = range?.from as Date;
+  const checkOut = range?.to as Date;
+
+  if (!userId) {
+    return <SignInButton mode='modal'>
+      <Button type='button' className="w-full">
+        Sign In to Complete Booking
+      </Button>
+    </SignInButton>
+  }
+
+  const createBooking = createBookingAction.bind(null, {
+    propertyId,
+    checkIn,
+    checkOut
+  });
+
   return (
-    <div>ConfirmBooking</div>
+    <section>
+      <FormContainer action={createBooking}>
+        <SubmitButton text='Reserve' className="w-full" />
+      </FormContainer>
+    </section>
   )
 }

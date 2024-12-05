2c9730b6d3a7a26cad35979cc84eaa3c5900ec29
Marcus <moliveira@pinterest.com>
2024-12-05 09:50:36 -0800

    fix: :bug: fix vercel deploy

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 import db from "@/utils/db";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { formatDate } from "@/utils/format";
 
 export const POST = async (req: NextRequest) => {
